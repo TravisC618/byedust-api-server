9f644c28d2a703c76990cd15b3f9c48c51b7aae9
travisC <travis_zg@outlook.com>
2020-02-29 17:43:02 +0700

    changed ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,33 +9,11 @@ module.exports = {
       watch: false,
       max_memory_restart: "1G",
       env: {
-        NODE_ENV: "development",
-        PORT: 4000,
-        DB_HOST: "localhost",
-        DB_PORT: 27017,
-        DB_DATABASE: "handy-booking-app"
+        NODE_ENV: "development"
       },
       env_production: {
-        NODE_ENV: "production",
-        PORT: 4000,
-        DB_USER: "admin",
-        DB_PASSWORD: "project3",
-        DB_HOST: "cluster0-rkmho.mongodb.net",
-        DB_DATABASE: "handy-booking-app",
-        JWT_KEY:
-          "jYa88LWNX2t3wlHhZMCHJyb7BBKZ_mGvEwfb2evgWvTpSjkWW-GSw8n-Qa2wGmDXFeeqp579DJCJ_6esveCct0k2G1koppXVpMTYa_rokq32MMe0VFSYcpVwxTT7a"
+        NODE_ENV: "production"
       }
     }
   ]
-
-  // deploy : {
-  //   production : {
-  //     user : 'node',
-  //     host : '212.83.163.1',
-  //     ref  : 'origin/master',
-  //     repo : 'git@github.com:repo.git',
-  //     path : '/var/www/production',
-  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-  //   }
-  // }
 };
