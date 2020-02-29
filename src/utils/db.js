404150b6dce4092ae90f97f0574c34cc56db98d8
travisC <travis_zg@outlook.com>
2020-02-29 16:01:16 +0700

    added deploy config

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,15 +2,17 @@ const mongoose = require("mongoose");
 
 exports.connectToDB = () => {
   const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } = process.env;
+  // mongoose.set('debug', true);
 
+  // 检查 DB_USER和DB_PASSWORD or process.env.NODE_ENV是否为production
   let connectionString;
-  // or. process.env.NODE_ENV === "production"
   if (DB_USER && DB_PASSWORD) {
     connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`;
   } else {
     connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
   }
-  console.log(`Connection to ${connectionString}`);
+
+  console.log(`Connecting to ${connectionString}`);
   mongoose.set("useFindAndModify", false);
   return mongoose.connect(connectionString, {
     useNewUrlParser: true,
