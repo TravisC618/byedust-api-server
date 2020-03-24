743e848663acaff80e5a9cf7a77720fbc523dd73
travisC <travis_zg@outlook.com>
2020-03-25 00:07:55 +1000

    impleted socketio demo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const socketio = require("socket.io");
 const htttp = require("http");
 require("express-async-errors");
 const cors = require("cors");
+const { addUser, getUser, removeUser } = require("./controllers/chat");
 
 const routers = require("./routes");
 const { connectToDB } = require("./utils/db");
@@ -19,14 +20,42 @@ app.use(cors());
 app.use(express.json());
 
 io.on("connection", socket => {
-  console.log(`we have a new connection`);
-
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit("message", {
+      user: "Byedust admin",
+      text: `${user.name}, welcome to room ${user.room}.`
+    });
+    socket.broadcast.to(user.room).emit("message", {
+      user: "Byedust admin",
+      text: `${user.name} has joined!`
+    });
+
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
   });
 
   socket.on("disconnect", () => {
-    console.log("User has left.");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Byedust admin",
+        text: `${user.name} has left.`
+      });
+    }
   });
 });
 
